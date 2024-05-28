66e8509df03ac40913f9b8064af9bad747d14c8d
sakshiandhale12 <sakshiandhale12@gmail.com>
2024-05-28 11:40:08 +0530

    new

---
+++ sakshi_workspace/python_my_code/Yard_project/env/lib/python3.8/site-packages/mylibrary/__init__.py
@@ -0,0 +1,5 @@
+ # mylibrary/__init__.py
+
+from .mymodule import hello_world
+from .mymodule import hello_world2
+
---
+++ sakshi_workspace/python_my_code/Yard_project/env/lib/python3.8/site-packages/mylibrary/mymodule.py
@@ -0,0 +1,7 @@
+# mylibrary/mymodule.py
+
+def hello_world():
+    return "Hello, Abhinesh! Nice To Meet You"
+
+def hello_world2():
+    return "Hello, World2! Nice To Meet You"
